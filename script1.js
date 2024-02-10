5295ad91e20a3542f9a2d657224dc184e9a12c77
chetna-mandloi <158042058+chetna-mandloi@users.noreply.github.com>
2024-02-10 12:08:53 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,22 @@
+const Locoscroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    smartphone:{
+        smooth: true
+    }
+});
+
+
+
+
+
+
+
+
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('data-scroll-container'),
+//     smooth: true,
+//     smartphone:{
+//         smooth:true
+//     }
+// });
